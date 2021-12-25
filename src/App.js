16da282b18f825ed8a2370570ed098feff89fe2b
bored-wizard <sujanbarman12345q@gmail.com>
2021-12-25 16:43:59 +0530

    added something

--- src/App.js
+++ src/App.js
@@ -65,8 +65,11 @@ const App = () => {
     }
   }
   const handleHide = index => {
-    const ele = document.getElementById(`hide${index}`);
+    const ele0 = document.getElementById(`hide${index}`);
+    const ele = document.getElementById(`vidLoad${index}`)
+    console.log("loaded")
     ele.className += "hide"
+    ele0.className += "hide"
   }
 
   const handleLike = index => {
@@ -225,8 +228,38 @@ const App = () => {
 
   }, [videoList, loadingVideo])
 
+  const handleLoading = (index, e) => {
+    // console.log("loading")
+    // const video = document.getElementById(`video${index}`)
+    // const ele = document.getElementById(`vidLoad${index}`)
+    // if (video.buffered.length === 0) {
+    //   ele.className += "hide";
+    // }
+    // console.log(video.buffered.length)
+  }
+
+  // const handleLoaded = (index, e) => {
+  //   console.log("loaded")
+  //   const ele = document.getElementById(`vidLoad${index}`)
+  //   if (ele) {
+  //     ele.className -= "hide"
+  //   }
+  // }
+
+  const handleFollow = (index) => {
+    const ele = document.getElementById(`userFoloweArea${index}`)
+    console.log("plk")
+    if (ele) {
+      if(ele.innerText === "Following"){
+        ele.innerText = "Follow"
+      }else if(ele.innerText === "Follow"){
+        ele.innerText = "Following"
+      }
+    }
+  }
+
   const handleVideoError = e => {
-    console.log({...e});
+    console.log({ ...e });
   }
   return (
     <div className="Container noScrollbar" onScroll={e => handleScroll(e)} >
@@ -236,7 +269,7 @@ const App = () => {
             <div
               ref={el => targetRef.current[index] = el}
               key={index} id={String(index)} className=" bg-gray-500 sn relative "  >
-              <video onError={e => handleVideoError(e)} onLoadStart={() => {}} onLoadedDataCapture={() => {}} onLoadedData={() => { handleHide(index) }} onClick={(e) => { handlePausePlay(String(index), e) }} className="videoo" autoPlay={false} height="100%" preload="auto" loop={true} >
+              <video preload="auto" id={`video${index}`} onError={e => handleVideoError(e)} onWaiting={(e) => { handleLoading(String(index), e) }} onLoadedData={() => { handleHide(index) }} onClick={(e) => { handlePausePlay(String(index), e) }} className="videoo" autoPlay={false} height="100%" preload="auto" loop={true} >
                 <source src={item.link} type="video/mp4" />
               </video>
               <div className="ratingOverlay" >
@@ -244,13 +277,30 @@ const App = () => {
                 <FaThumbsDown onClick={() => { handleDislike(String(index)) }} id={`dislike${index}`} className="dislike" size={27} />
                 <FaRegComment onClick={() => { handleComment(String(index)) }} id={`comment${index}`} className="comment" size={27} />
                 <FaEllipsisH onClick={() => { handleDescription(String(index)) }} id={`description${index}`} className="description" size={27} />
+                <div className="userdparea" >
+                  <ReactLoading type="cylon" color="white" className="loader" width={"20px"} />
+                </div>
               </div>
               <div id={`userDescription${index}`} className="UserDescriptionOverlay" onClick={() => handleUserDescription(String(index))} >
-                <span>James Steven Donaldson (born May 7, 1998), better known by his online alias MrBeast (formerly MrBeast6000), is an American YouTuber, businessman, and philanthropist. He has been credited with pioneering a genre of YouTube videos that center on expensive stunts.</span>
+                <div className="userDataArea" >
+                  <div className="userProfileImageArea" >
+                    <img src="https://www.w3schools.com/images/lamp.jpg" alt="" height="25px" width="25px"></img>
+                  </div>
+                  <span className="userNameArea" >Sujan barman</span>
+                  <div id={`userFoloweArea${index}`} className="userFoloweArea" onClick={handleFollow(String(index))} >
+                    <span onClick={handleFollow(String(index))}  >
+                      Following
+                    </span>
+                  </div>
+                </div>
+                <span>James Steven Donaldson (born May 7, 1998), better known by his online alias MrBeast (formerly MrBeast6000), is an American YouTuber, businessman, and philanthropist.</span>
               </div>
-              <div id={`hide${index}`}  className="upperBoss is-loading " >
+              <div id={`hide${index}`} className="upperBoss is-loading " >
                 <div className="userDes"></div>
-              </div> 
+              </div>
+              <div className="videoLoading" id={`vidLoad${index}`}>
+                <ReactLoading type="bubbles" color="gray" className="loader" width={"80px"} />
+              </div>
             </div>
           )
         })
